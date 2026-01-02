25f9c43498177877cc9d0f3236e45074eac9f06b
Anas <moxdanas414@gmail.com>
2026-01-02 18:11:32 +0530

    added env package till now

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(" CYBORG  IN THE CHAT !!!!!!!");
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+let myusername = process.env.anasname;
+let db = process.env.DB_PASSWORD;
+console.log("VALUE ", myusername);
+console.log(" database ", db);
