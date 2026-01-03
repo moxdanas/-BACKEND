141ef8082f8cf22fe8e134513b2fe57ef919b6a8
Anas <moxdanas414@gmail.com>
2026-01-03 16:21:27 +0530

    setted up express server and installed postman

--- index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({
-  path: "./.env",
-});
-let myusername = process.env.anasname;
-let db = process.env.DB_PASSWORD;
-console.log("VALUE ", myusername);
-console.log(" database ", db);
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page !");
+});
+app.get("/about", (reques, response) => {
+  response.`send("Welcome to About Page.");
+});
+export default app;
