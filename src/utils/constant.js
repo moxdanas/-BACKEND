31c1506245cfbfba1491418b7774e4669812c1cb
Anas <moxdanas414@gmail.com>
2026-01-06 17:01:50 +0530

    added constant.js file

---
+++ src/utils/constant.js
@@ -0,0 +1,14 @@
+export const userRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+};
+export const AvailableUserRoles = Object.values(userRolesEnum);
+// console.log(AvailableUserRoles)
+export const TaskStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done",
+};
+export const AvailableTaskStatuses = Object.values(TaskStatusEnum);
+// console.log(AvailableTaskStatuses)
