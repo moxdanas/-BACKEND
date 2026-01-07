559850d22fffab4157c76030d092f918b4102959
Anas <moxdanas414@gmail.com>
2026-01-07 22:21:35 +0530

    worked on user model schema using mongoose

--- src/controllers/healthCheck.controllers.js
+++ src/controllers/healthCheck.controllers.js
@@ -1,4 +1,6 @@
 import { ApiResponse } from "../utils/api-response.js";
+import asyncHandler from "../utils/aync-handler.js"
+/*
 const healthCheck = (req, res) => {
   try {
     res
@@ -6,4 +8,10 @@ const healthCheck = (req, res) => {
       .json(new ApiResponse(200, { message: "Server is Running . . ." }));
   } catch (error) {}
 };
+*/
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { message: "server is still running bitch 🙈" }));
+});
 export default healthCheck;
---
+++ src/models/user.models.js
@@ -0,0 +1,61 @@
+import mongoose, { Scheman } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: ``,
+        localPath: ``,
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required !"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
