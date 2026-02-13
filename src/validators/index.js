e0272f7ba1b90304ae5fd3d05d41bf85762e12b2
Anas <moxdanas414@gmail.com>
2026-02-13 17:54:05 +0530

    Lec134 , Wrote customised validations

---
+++ src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("username is required")
+      .isLowercase()
+      .withMessage("Username must in lowercase")
+      .isLength({ min: 3 })
+      .withMessage("username must be atleast 3 characters long"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("fullName").optional().trim(), 
+  ];
+};
+
+export { userRegisterValidator };
