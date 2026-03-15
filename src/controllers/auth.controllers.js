b5c0d123c3436b32fe53737303e68e6ca7c32f52
Anas <moxdanas414@gmail.com>
2026-03-16 03:23:18 +0530

    test

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -262,4 +262,11 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-
+export {
+  registerUser,
+  login,
+  logoutUser,
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+};
